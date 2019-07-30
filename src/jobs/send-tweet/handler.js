3b01ad4e289519a3033ac6de837d5f4ff283a948
Corbin Page <corbin.page@gmail.com>
2019-07-30 19:34:20 -0400

    Updated DAI tweets from 20-50k

--- src/jobs/send-tweet/handler.js
+++ src/jobs/send-tweet/handler.js
@@ -26,7 +26,7 @@ module.exports.start = async (event) => {
 	let confirmSendMessage = false
 	let message = ''
 
-	if(botani.params.tokenSymbol === 'DAI' && botani.params.amount >= 20000) {
+	if(botani.params.tokenSymbol === 'DAI' && botani.params.amount >= 50000) {
 		confirmSendMessage = true
 	} else if(botani.params.tokenSymbol === 'MKR' && botani.params.amount >= 20) {
 		confirmSendMessage = true
